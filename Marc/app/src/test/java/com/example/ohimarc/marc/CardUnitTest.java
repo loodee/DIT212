e2a6093a3a777a828ac76fd19b7e4a778c11e936
galbertsson <gustav.albertsson@hotmail.com>
2018-09-19 16:00:56 +0200

    Added unit tests for Card class

---
+++ Marc/app/src/test/java/com/example/ohimarc/marc/CardUnitTest.java
@@ -0,0 +1,34 @@
+package com.example.ohimarc.marc;
+
+import com.example.ohimarc.marc.Model.Card;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CardUnitTest {
+    @Test
+    public void createCard() {
+        Card c = new Card("Front", "Back");
+
+        assertEquals(c.getFront(), "Front");
+        assertEquals(c.getBack(), "Back");
+    }
+
+    @Test
+    public void createNullBack() {
+        Card c = new Card("Front", null);
+
+        assertEquals(c.getFront(), "Front");
+        assertEquals(c.getBack(), "");
+    }
+
+    @Test
+    public void createNullFront() {
+        Card c = new Card(null, "Back");
+
+        assertEquals(c.getFront(), "");
+        assertEquals(c.getBack(), "Back");
+    }
+
+}
