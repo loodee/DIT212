7dfedfe0c7c176111319b03f8037b8ab9a55652e
galbertsson <gustav.albertsson@hotmail.com>
2018-09-19 16:00:11 +0200

    Added error handling for Card constructor

--- Marc/app/src/main/java/com/example/ohimarc/marc/Model/Card.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/Model/Card.java
@@ -1,10 +1,27 @@
 package com.example.ohimarc.marc.Model;
 
+/**
+ * A class that holds the information for a Card, it holds the front and the back of the card.
+ * */
 public class Card {
 
     private String front, back;
-    
+
+    /**
+    * Creates a card with front and back,
+     * if front or back is null they will be set to empty string
+     *
+     * @param front The front on the card
+     * @param back The backside of the card
+    * */
     public Card(String front, String back){
+        if(front == null){
+            front = "";
+        }
+        if(back == null){
+            back = "";
+        }
+
         this.front = front;
         this.back = back;
     }
