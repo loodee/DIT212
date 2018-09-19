34ef4e12eb4c4741fdff5538e8410f9ebfecbec0
Unknown <victor.c.johansson@gmail.com>
2018-09-19 16:59:39 +0200

    Added functionality for flashcard
    You can now press the card and the text will be changed

--- Marc/app/src/main/java/com/example/ohimarc/marc/Activity/FlashcardActivity.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/Activity/FlashcardActivity.java
@@ -2,19 +2,30 @@ package com.example.ohimarc.marc.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
+import com.example.ohimarc.marc.Model.Card;
 import com.example.ohimarc.marc.R;
 
 public class FlashcardActivity extends AppCompatActivity {
 
+    Card test = new Card("Test","Test2");
+    Button button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_flashcard);
+        button = (Button)findViewById(R.id.button2);
+        button.setText(test.getFront());
     }
 
     public void flashCardClicked(View view){
+        //Log.d("test","FLASHCARD CLICKED");
+        button.setText(test.getBack());
 
     }
 
