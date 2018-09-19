e6d1de298dc39e8d8464fc37d946187c98adbf40
galbertsson <gustav.albertsson@hotmail.com>
2018-09-19 14:42:02 +0200

    Implemented Card class base, variables and getters
    Co-authored-by: loodee <guslithd@student.gu.se>

--- Marc/app/src/main/java/com/example/ohimarc/marc/Model/Card.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/Model/Card.java
@@ -1,4 +1,20 @@
 package com.example.ohimarc.marc.Model;
 
 public class Card {
+
+    private String front, back;
+    
+    public Card(String front, String back){
+        this.front = front;
+        this.back = back;
+    }
+
+    public String getFront() {
+        return front;
+    }
+
+    public String getBack() {
+        return back;
+    }
+
 }
