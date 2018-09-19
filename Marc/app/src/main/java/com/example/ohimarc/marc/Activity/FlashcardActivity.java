2e8805bd1c57c8847f3e550ced154d785b1c477a
Unknown <victor.c.johansson@gmail.com>
2018-09-19 15:46:46 +0200

    Creation of files and bugfixes
    Created map and file for activites but also fixed the bug so the preview can be used(see changes in styles.xml for bugfix)

---
+++ Marc/app/src/main/java/com/example/ohimarc/marc/Activity/FlashcardActivity.java
@@ -0,0 +1,22 @@
+package com.example.ohimarc.marc.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.ohimarc.marc.R;
+
+public class FlashcardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void flashCardClicked(View view){
+
+    }
+
+
+}
