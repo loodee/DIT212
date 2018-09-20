389df522caa577c84084bb1b091a5d426b9ee0c3
Alex <axoomax94@hotmail.com>
2018-09-20 17:13:29 +0200

    Added modifications to flashcard view
    The view's toolbar got replaces with an actual toolbar. The FlashcarActivity received some modifications to support the new toolbar.

--- Marc/app/src/main/java/com/example/ohimarc/marc/Activity/FlashcardActivity.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/Activity/FlashcardActivity.java
@@ -4,16 +4,18 @@
  */
 package com.example.ohimarc.marc.Activity;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.ohimarc.marc.Model.*;
 import com.example.ohimarc.marc.R;
-
-import org.w3c.dom.Text;
 
 public class FlashcardActivity extends AppCompatActivity {
 
@@ -34,13 +36,19 @@ public class FlashcardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashcard);
-        cardButton = (Button)findViewById(R.id.cardButton);
-        cardTitle = (TextView)findViewById(R.id.cardTitle);
-        deckTitle = (TextView)findViewById(R.id.deckTitle);
+        cardTitle = findViewById(R.id.cardTitle);
         cardTitle.setText("Q:");
+        cardButton = findViewById(R.id.cardButton);
         testDeck.addCard(test.getFront(),test.getBack());
         cardButton.setText(testGame.peekNextCard()[0]);
-        deckTitle.setText(testGame.getDeckTitle());
+        setSupportActionBar((Toolbar) findViewById(R.id.my_toolbar));
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle(testGame.getDeckTitle());
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_items,menu);
+        return true;
     }
 
     /**
