4fc9bb477f2c94195b83f71c73d53fdbd412bf34
Vroxie <victor.c.johansson@gmail.com>
2018-09-20 14:41:52 +0200

    Generalised code and commenting
    The code now works more like it should and no needed dependencies are no longer used

--- Marc/app/src/main/java/com/example/ohimarc/marc/Activity/FlashcardActivity.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/Activity/FlashcardActivity.java
@@ -1,3 +1,7 @@
+/**
+ * Author: Victor Johansson githubnice: Vroxie
+ * Last updated 20/9 2018
+ */
 package com.example.ohimarc.marc.Activity;
 
 import android.support.v7.app.AppCompatActivity;
@@ -21,6 +25,11 @@ public class FlashcardActivity extends AppCompatActivity {
     private FlashCardGame testGame = new FlashCardGame(testDeck);
 
 
+    /**
+     * chooses which activity that will be shown and sets tex etc.
+     * @param savedInstanceState this is to start at last instance the app was in
+     * before the app were shut down
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -28,21 +37,28 @@ public class FlashcardActivity extends AppCompatActivity {
         cardButton = (Button)findViewById(R.id.cardButton);
         cardTitle = (TextView)findViewById(R.id.cardTitle);
         deckTitle = (TextView)findViewById(R.id.deckTitle);
-        cardTitle.setText("Q");
+        cardTitle.setText("Q:");
         testDeck.addCard(test.getFront(),test.getBack());
-        cardButton.setText(testDeck.getCardInfo(0)[0]);
-        deckTitle.setText(testDeck.getTitle());
+        cardButton.setText(testGame.peekNextCard()[0]);
+        deckTitle.setText(testGame.getDeckTitle());
     }
 
+    /**
+     * checking which "side" that is showing i.e either the question or the answer
+     * So depening on which "side" that is shown when the card is pressed
+     * the method "flips" the card
+     * @param view telling which view that is clicked
+     */
     public void flashCardClicked(View view){
         //Log.d("test","FLASHCARD CLICKED");
-        if(cardButton.getText().equals(testDeck.getCardInfo(0)[0])) {
-            cardButton.setText(testDeck.getCardInfo(0)[1]);
-            cardTitle.setText("A");
+        boolean isQuestion = cardButton.getText().equals(testGame.peekNextCard()[0]);
+        if(isQuestion) {
+            cardButton.setText(testGame.peekNextCard()[1]);
+            cardTitle.setText("A:");
         }
         else{
-            cardButton.setText(testDeck.getCardInfo(0)[0]);
-            cardTitle.setText("Q");
+            cardButton.setText(testGame.peekNextCard()[0]);
+            cardTitle.setText("Q:");
         }
     }
 
