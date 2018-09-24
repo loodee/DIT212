af5071815b1a070e228a2af474fa43e1d9f3dfc5
Vroxie <victor.c.johansson@gmail.com>
2018-09-24 13:35:27 +0200

    Added testdecks and testcards etc
    Added testdecks and testcards to see if it all works, also implemented flashCardClicked

--- Marc/app/src/main/java/com/example/ohimarc/marc/Presenter/FlashcardPresenter.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/Presenter/FlashcardPresenter.java
@@ -1,10 +1,29 @@
 package com.example.ohimarc.marc.Presenter;
 
+import com.example.ohimarc.marc.Model.Card;
+import com.example.ohimarc.marc.Model.Deck;
+import com.example.ohimarc.marc.Model.FlashCardGame;
+import com.example.ohimarc.marc.View.FlashcardView;
+
 public class FlashcardPresenter implements Presenter {
 
+    private FlashCardGame game;
+    private FlashcardView view;
+    private Deck testDeck;
+
+
+    public FlashcardPresenter(FlashcardView view){
+        Card testCard1 = new Card("Hej","Alexander");
+        Card testCard2 = new Card("Hej","Victor");
+        testDeck = new Deck("testdeck");
+        testDeck.addCard(testCard1.getFront(),testCard1.getBack());
+        testDeck.addCard(testCard2.getFront(),testCard2.getBack());
+        this.view = view;
+        game = new FlashCardGame(testDeck);
+    }
     @Override
     public void onCreate() {
-
+        game = new FlashCardGame(testDeck);
     }
 
     @Override
@@ -20,5 +39,15 @@ public class FlashcardPresenter implements Presenter {
     @Override
     public void onDestroy() {
 
+    }
+
+
+    public void flashCardClicked(boolean frontActive){
+        if(frontActive){
+            view.flipCardButton("A:",game.peekNextCard()[1]);
+        }
+        else{
+            view.flipCardButton("Q:",game.peekNextCard()[0]);
+        }
     }
 }
