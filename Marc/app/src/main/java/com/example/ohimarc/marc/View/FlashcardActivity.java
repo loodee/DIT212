0b49c6b5fce6bf57dae078565fb7cf11e7cce28d
Alex <axoomax94@hotmail.com>
2018-09-24 13:58:43 +0200

    Adding modified Flashcard View and Activity
    Made major changes to the activity file and minor changes to the view.

--- Marc/app/src/main/java/com/example/ohimarc/marc/View/FlashcardActivity.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/View/FlashcardActivity.java
@@ -14,61 +14,75 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.ohimarc.marc.Model.*;
+import com.example.ohimarc.marc.Presenter.FlashcardPresenter;
 import com.example.ohimarc.marc.R;
 
-public class FlashcardActivity extends AppCompatActivity {
+public class FlashcardActivity extends AppCompatActivity implements FlashcardView {
 
-    private Card test = new Card("Test","Test2");
+
     private Button cardButton;
     private TextView cardTitle;
-    private TextView deckTitle;
-    private Deck testDeck = new Deck("testDeck");
-    private FlashCardGame testGame = new FlashCardGame(testDeck);
 
+    FlashcardPresenter presenter = new FlashcardPresenter(this);
 
-    /**
-     * chooses which activity that will be shown and sets tex etc.
-     * @param savedInstanceState this is to start at last instance the app was in
-     * before the app were shut down
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashcard);
         cardTitle = findViewById(R.id.cardTitle);
-        cardTitle.setText("Q:");
         cardButton = findViewById(R.id.cardButton);
-        testDeck.addCard(test.getFront(),test.getBack());
-        cardButton.setText(testGame.peekNextCard()[0]);
+        cardTitle.setText("Q:");
         setSupportActionBar((Toolbar) findViewById(R.id.my_toolbar));
-        ActionBar ab = getSupportActionBar();
-        ab.setTitle(testGame.getDeckTitle());
+        presenter.onCreate();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        presenter.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        presenter.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        presenter.onDestroy();
+    }
+
+    public void setTitle(String deckTitleText) {
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle(deckTitleText);
+    }
+
+    public void flipCardButton(String qora, String text) {
+        cardTitle.setText(qora);
+        cardButton.setText(text);
+    }
+
+    public void flipCardButtonClicked(View v) {
+        boolean bool = false;
+        if(cardTitle.getText().equals("Q:")) {
+            bool = true;
+        }
+        presenter.flashCardClicked(bool);
+    }
+
+    public void correctButton() {
+
+    }
+
+    public void incorrectButton() {
+
+    }
+
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.toolbar_items,menu);
         return true;
     }
-
-    /**
-     * checking which "side" that is showing i.e either the question or the answer
-     * So depening on which "side" that is shown when the card is pressed
-     * the method "flips" the card
-     * @param view telling which view that is clicked
-     */
-    public void flashCardClicked(View view){
-        //Log.d("test","FLASHCARD CLICKED");
-        boolean isQuestion = cardButton.getText().equals(testGame.peekNextCard()[0]);
-        if(isQuestion) {
-            cardButton.setText(testGame.peekNextCard()[1]);
-            cardTitle.setText("A:");
-        }
-        else{
-            cardButton.setText(testGame.peekNextCard()[0]);
-            cardTitle.setText("Q:");
-        }
-    }
-
-
 }
