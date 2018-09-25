dcdb08841e21dd37d79cf55368055342df6fd1ac
galbertsson <gustav.albertsson@hotmail.com>
2018-09-25 12:52:27 +0200

    Added toolbar to main menu

--- Marc/app/src/main/java/com/example/ohimarc/marc/Home.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/Home.java
@@ -2,6 +2,9 @@ package com.example.ohimarc.marc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setSupportActionBar((Toolbar) findViewById(R.id.my_toolbar));
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_items,menu);
+        return true;
     }
 }
