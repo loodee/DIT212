68328de59c32ed05a3497dfb55be383c36b9b7ca
Thomas Li <guslithd@student.gu.se>
2018-09-28 15:53:38 +0200

    Added skeleton Note editing screen

---
+++ Marc/app/src/main/java/com/example/ohimarc/marc/view/EditNoteActivity.java
@@ -0,0 +1,28 @@
+package com.example.ohimarc.marc.view;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.example.ohimarc.marc.R;
+
+public class EditNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_note);
+        setSupportActionBar((Toolbar) findViewById(R.id.my_toolbar));
+        ActionBar bar = getSupportActionBar();
+        bar.setTitle("Edit Note");
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_items,menu);
+        return true;
+    }
+}
