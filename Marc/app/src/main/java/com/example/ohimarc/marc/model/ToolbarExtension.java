8f8b2602560049cf623c395e12d9bf14ddf14a10
Alex <axoomax94@hotmail.com>
2018-10-01 15:28:31 +0200

    Adding abstract class
    ToolbarExtension (TE) extends the AppCompatActivity class. TE contains a few functions which makes it easier to implement the toolbar if needed. This reduces our "copy paste programming".

---
+++ Marc/app/src/main/java/com/example/ohimarc/marc/model/ToolbarExtension.java
@@ -0,0 +1,37 @@
+package com.example.ohimarc.marc.model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.TextView;
+
+import com.example.ohimarc.marc.R;
+
+abstract public class ToolbarExtension extends AppCompatActivity {
+
+    public TextView titleText;
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_items,menu);
+        return true;
+    }
+
+    public void initiateToolbar(String title) {
+        Toolbar tb = findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+        tb.setNavigationIcon(R.drawable.ic_menu_black_24dp);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        titleText = findViewById(R.id.toolbar_text);
+        titleText.setText(title);
+    }
+
+    public void initiateToolbar() {
+        Toolbar tb = findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+        tb.setNavigationIcon(R.drawable.ic_menu_black_24dp);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        titleText = findViewById(R.id.toolbar_text);
+    }
+}
