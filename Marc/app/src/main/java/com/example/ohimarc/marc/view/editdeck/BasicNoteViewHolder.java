1ff2a78b8866e1bf1671f259233420cd000563aa
Sorchar <gusmathfo@student.gu.se>
2018-10-03 15:35:24 +0200

    Updated EditTexts with TextViews
    Co-authored-by: Sorchar <gusmathfo@student.gu.se> 
    Co-authored-by: loodee <guslithd@student.gu.se>

--- Marc/app/src/main/java/com/example/ohimarc/marc/view/editdeck/BasicNoteViewHolder.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/view/editdeck/BasicNoteViewHolder.java
@@ -4,18 +4,19 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.ohimarc.marc.R;
 
 public class BasicNoteViewHolder extends RecyclerView.ViewHolder {
 
-    private EditText basicNoteTextFront;
-    private EditText basicNoteTextBack;
+    private TextView basicNoteTextFront;
+    private TextView basicNoteTextBack;
 
     public BasicNoteViewHolder(@NonNull View itemView) {
         super(itemView);
-        basicNoteTextFront = itemView.findViewById(R.id.et_front_basic_note);
-        basicNoteTextBack = itemView.findViewById(R.id.et_back_basic_note);
+        basicNoteTextFront = itemView.findViewById(R.id.tv_front_basic_note);
+        basicNoteTextBack = itemView.findViewById(R.id.tv_back_basic_note);
     }
 
     public void setBasicNoteText(String frontText, String backText) {
