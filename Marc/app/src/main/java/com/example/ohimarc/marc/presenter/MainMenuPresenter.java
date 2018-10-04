8396ea1c1d640e6ba28d98c6f60136aa191f2f60
galbertsson <gustav.albertsson@hotmail.com>
2018-10-04 16:46:56 +0200

    Removed unused imports

--- Marc/app/src/main/java/com/example/ohimarc/marc/presenter/MainMenuPresenter.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/presenter/MainMenuPresenter.java
@@ -1,16 +1,12 @@
 package com.example.ohimarc.marc.presenter;
 
-import android.support.v7.widget.RecyclerView;
 
 import com.example.ohimarc.marc.model.User;
 import com.example.ohimarc.marc.service.LocalUserStorage;
 import com.example.ohimarc.marc.service.UserStorage;
-import com.example.ohimarc.marc.view.mainMenu.StartMenuActivity;
 import com.example.ohimarc.marc.view.mainMenu.StartMenuContract;
 import com.example.ohimarc.marc.view.mainMenu.UserViewHolder;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainMenuPresenter implements StartMenuContract.Presenter {
