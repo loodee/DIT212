28278ede68398d8e857d74905ac724f32d62d86d
galbertsson <gustav.albertsson@hotmail.com>
2018-10-05 12:35:31 +0200

    Added handling users to MemorizationTrainingTool class

--- Marc/app/src/main/java/com/example/ohimarc/marc/model/MemorizationTrainingTool.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/model/MemorizationTrainingTool.java
@@ -1,4 +1,29 @@
 package com.example.ohimarc.marc.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MemorizationTrainingTool {
+
+    private static final MemorizationTrainingTool INSTANCE = new MemorizationTrainingTool();
+
+    public static MemorizationTrainingTool getInstance(){
+        return INSTANCE;
+    }
+
+    private List<User> users = new ArrayList<>();
+    private int activeUser;
+
+    public User getActiveUser() {
+        return users.get(activeUser);
+    }
+
+    public void addNewUser(String name){
+        users.add(new User(name));
+    }
+
+    public void setActiveUser(int id){
+        activeUser = id;
+    }
+
 }
