d10eeb1b576cd4c8b641313b01fc735d05d035a3
galbertsson <gustav.albertsson@hotmail.com>
2018-10-08 20:03:17 +0200

    Added missing import for Toast and Intent to StartMenuActivity

--- Marc/app/src/main/java/com/example/ohimarc/marc/view/mainMenu/StartMenuActivity.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/view/mainMenu/StartMenuActivity.java
@@ -1,6 +1,7 @@
 package com.example.ohimarc.marc.view.mainMenu;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.ohimarc.marc.R;
 import com.example.ohimarc.marc.presenter.MainMenuPresenter;
