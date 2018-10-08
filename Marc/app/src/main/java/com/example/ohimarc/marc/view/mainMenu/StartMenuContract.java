5f998e1734a7d7721dafa19889b4d9b70c3d5c4e
galbertsson <gustav.albertsson@hotmail.com>
2018-10-08 21:41:50 +0200

    Removed un-used interfaces

--- Marc/app/src/main/java/com/example/ohimarc/marc/view/mainMenu/StartMenuContract.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/view/mainMenu/StartMenuContract.java
@@ -1,18 +1,6 @@
 package com.example.ohimarc.marc.view.mainMenu;
 
-import android.support.v7.widget.RecyclerView;
-
 public interface StartMenuContract {
-    interface View {
-        void setPresenter(StartMenuContract.Presenter presenter);
-
-        void updateDeckList();
-
-        void deckTitles();
-
-        void editDeckTitle();
-    }
-
     interface Presenter {
         void start();
 
