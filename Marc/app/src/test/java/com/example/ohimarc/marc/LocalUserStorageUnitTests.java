00963ee20a78f0687bfee9f5dcd4e58bf7d0812a
galbertsson <gustav.albertsson@hotmail.com>
2018-10-08 21:38:30 +0200

    Updated unit tests for LocalUserStorage

--- Marc/app/src/test/java/com/example/ohimarc/marc/LocalUserStorageUnitTests.java
+++ Marc/app/src/test/java/com/example/ohimarc/marc/LocalUserStorageUnitTests.java
@@ -1,5 +1,6 @@
 package com.example.ohimarc.marc;
 
+import com.example.ohimarc.marc.model.MemorizationTrainingTool;
 import com.example.ohimarc.marc.model.User;
 import com.example.ohimarc.marc.service.LocalUserStorage;
 
@@ -18,67 +19,68 @@ import java.util.List;
 
 public class LocalUserStorageUnitTests {
 
+
     private LocalUserStorage storage;
-    private List<User> users;
+    private MemorizationTrainingTool mtt;
+    private MemorizationTrainingTool stored;
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
     @Before
     public void setup(){
-
+        mtt = new MemorizationTrainingTool();
         try {
             storage = new LocalUserStorage(folder.newFolder("test").getAbsolutePath());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        users = new ArrayList<>();
     }
 
     @Test
     public void readNoFile() {
-        List<User> usersFromStore = storage.getStoredUsers();
+        mtt = storage.getStoredState();
 
-        assertEquals(usersFromStore.size(), 0);
+        assertEquals(mtt.getUsers().size(), 0);
     }
 
     @Test
     public void saveUser() {
-        users.add(new User("Bob"));
+        mtt.addNewUser("Bob");
 
-        storage.storeUsers(users);
-        List<User> usersFromStore = storage.getStoredUsers();
+        storage.storeState(mtt);
+        stored = storage.getStoredState();
 
-        assertEquals(users.get(0).getName(), usersFromStore.get(0).getName());
+        assertEquals("Bob", stored.getUsers().get(0).getName());
     }
 
     @Test
     public void saveNullUserList() {
-        storage.storeUsers(null);
-        List<User> usersFromStore = storage.getStoredUsers();
+        storage.storeState(null);
+        stored = storage.getStoredState();
 
-        assertEquals(usersFromStore.size(), 0);
+        assertEquals(0, stored.getUsers().size());
     }
 
     @Test
     public void saveEmptyList() {
-        storage.storeUsers(users);
-        List<User> usersFromStore = storage.getStoredUsers();
+        storage.storeState(mtt);
+        stored = storage.getStoredState();
 
-        assertEquals(usersFromStore.size(), 0);
+        assertEquals(stored.getUsers().size(), 0);
     }
 
     @Test
     public void saveSeveralUsers() {
-        users.add(new User("Bob"));
-        users.add(new User("Alice"));
+        mtt.addNewUser("Bob");
+        mtt.addNewUser("Alice");
 
-        storage.storeUsers(users);
-        List<User> usersFromStore = storage.getStoredUsers();
+        storage.storeState(mtt);
+        stored = storage.getStoredState();
 
-        assertEquals(users.size(), usersFromStore.size());
-        assertEquals(users.get(0).getName(), usersFromStore.get(0).getName());
-        assertEquals(users.get(1).getName(), usersFromStore.get(1).getName());
+        assertEquals(2, stored.getUsers().size());
+        assertEquals("Bob", stored.getUsers().get(0).getName());
+        assertEquals("Alice", stored.getUsers().get(1).getName());
     }
 
 }
