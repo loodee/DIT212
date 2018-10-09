d52cae4f0c0e98f9cca559792e2e14a328e4afdd
galbertsson <gustav.albertsson@hotmail.com>
2018-10-09 16:32:53 +0200

    Added getters and setters for handling decks in user, updated unit tests

--- Marc/app/src/test/java/com/example/ohimarc/marc/LocalUserStorageUnitTests.java
+++ Marc/app/src/test/java/com/example/ohimarc/marc/LocalUserStorageUnitTests.java
@@ -1,5 +1,6 @@
 package com.example.ohimarc.marc;
 
+import com.example.ohimarc.marc.model.Deck;
 import com.example.ohimarc.marc.model.MemorizationTrainingTool;
 import com.example.ohimarc.marc.model.User;
 import com.example.ohimarc.marc.service.LocalUserStorage;
@@ -83,4 +84,107 @@ public class LocalUserStorageUnitTests {
         assertEquals("Alice", stored.getUsers().get(1).getName());
     }
 
+    @Test
+    public void saveUserWithDeck() {
+        mtt.addNewUser("Bob");
+        mtt.setActiveUser(0);
+
+        User u = mtt.getActiveUser();
+        u.createNewDeck("Test Deck");
+
+        storage.storeState(mtt);
+        stored = storage.getStoredState();
+
+        assertEquals(1,stored.getActiveUser().getDeckTitles().size());
+        assertEquals(mtt.getActiveUser().getDeckTitles().get(0), stored.getActiveUser().getDeckTitles().get(0));
+    }
+
+    @Test
+    public void saveUserWithDecks() {
+        mtt.addNewUser("Bob");
+        mtt.setActiveUser(0);
+
+        User u = mtt.getActiveUser();
+        u.createNewDeck("Test Deck");
+        u.createNewDeck("Test Deck1");
+        u.createNewDeck("Test Deck2");
+
+        storage.storeState(mtt);
+        stored = storage.getStoredState();
+
+        assertEquals(3,stored.getActiveUser().getDeckTitles().size());
+        assertEquals(mtt.getActiveUser().getDeckTitles().get(0), stored.getActiveUser().getDeckTitles().get(0));
+        assertEquals(mtt.getActiveUser().getDeckTitles().get(1), stored.getActiveUser().getDeckTitles().get(1));
+        assertEquals(mtt.getActiveUser().getDeckTitles().get(2), stored.getActiveUser().getDeckTitles().get(2));
+    }
+
+    @Test
+    public void saveUserWithDeckAndCard() {
+        mtt.addNewUser("Bob");
+        mtt.setActiveUser(0);
+
+        User u = mtt.getActiveUser();
+        u.createNewDeck("Test Deck");
+
+        Deck d = u.getDeck(0);
+        d.addBasicNote("Some front", "Some back");
+
+        storage.storeState(mtt);
+        stored = storage.getStoredState();
+
+        assertEquals(1, stored.getActiveUser().getDeckTitles().size());
+        assertEquals(mtt.getActiveUser().getDeck(0).getCardCopies()[0][0], stored.getActiveUser().getDeck(0).getCardCopies()[0][0]);
+        assertEquals(mtt.getActiveUser().getDeck(0).getCardCopies()[0][1], stored.getActiveUser().getDeck(0).getCardCopies()[0][1]);
+    }
+
+    @Test
+    public void saveUserWithDeckAndCards() {
+        mtt.addNewUser("Bob");
+        mtt.setActiveUser(0);
+
+        User u = mtt.getActiveUser();
+        u.createNewDeck("Test Deck");
+
+        Deck d = u.getDeck(0);
+        d.addBasicNote("Some front", "Some back");
+        d.addBasicNote("Some front2", "Some back2");
+
+        storage.storeState(mtt);
+        stored = storage.getStoredState();
+
+        assertEquals(1, stored.getActiveUser().getDeckTitles().size());
+        assertEquals(mtt.getActiveUser().getDeck(0).getCardCopies()[0][0], stored.getActiveUser().getDeck(0).getCardCopies()[0][0]);
+        assertEquals(mtt.getActiveUser().getDeck(0).getCardCopies()[0][1], stored.getActiveUser().getDeck(0).getCardCopies()[0][1]);
+        assertEquals(mtt.getActiveUser().getDeck(0).getCardCopies()[1][0], stored.getActiveUser().getDeck(0).getCardCopies()[1][0]);
+        assertEquals(mtt.getActiveUser().getDeck(0).getCardCopies()[1][1], stored.getActiveUser().getDeck(0).getCardCopies()[1][1]);
+    }
+
+    @Test
+    public void saveUserWithDecksAndCards() {
+        mtt.addNewUser("Bob");
+        mtt.setActiveUser(0);
+
+        User u = mtt.getActiveUser();
+        u.createNewDeck("Test Deck");
+        u.createNewDeck("Test Deck2");
+
+        Deck d = u.getDeck(0);
+        d.addBasicNote("Some front", "Some back");
+        d.addBasicNote("Some front2", "Some back2");
+
+        d = u.getDeck(1);
+        d.addBasicNote("Front","Back");
+
+        storage.storeState(mtt);
+        stored = storage.getStoredState();
+
+        assertEquals(2, stored.getActiveUser().getDeckTitles().size());
+        assertEquals(mtt.getActiveUser().getDeck(0).getCardCopies()[0][0], stored.getActiveUser().getDeck(0).getCardCopies()[0][0]);
+        assertEquals(mtt.getActiveUser().getDeck(0).getCardCopies()[0][1], stored.getActiveUser().getDeck(0).getCardCopies()[0][1]);
+        assertEquals(mtt.getActiveUser().getDeck(0).getCardCopies()[1][0], stored.getActiveUser().getDeck(0).getCardCopies()[1][0]);
+        assertEquals(mtt.getActiveUser().getDeck(0).getCardCopies()[1][1], stored.getActiveUser().getDeck(0).getCardCopies()[1][1]);
+
+        assertEquals(mtt.getActiveUser().getDeck(1).getCardCopies()[0][0], stored.getActiveUser().getDeck(1).getCardCopies()[0][0]);
+        assertEquals(mtt.getActiveUser().getDeck(1).getCardCopies()[0][1], stored.getActiveUser().getDeck(1).getCardCopies()[0][1]);
+    }
 }
--- Marc/app/src/test/java/com/example/ohimarc/marc/UserUnitTest.java
+++ Marc/app/src/test/java/com/example/ohimarc/marc/UserUnitTest.java
@@ -1,11 +1,15 @@
 package com.example.ohimarc.marc;
 
 import com.example.ohimarc.marc.model.Card;
+import com.example.ohimarc.marc.model.Deck;
 import com.example.ohimarc.marc.model.User;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class UserUnitTest {
     @Test
@@ -14,7 +18,7 @@ public class UserUnitTest {
         
         User u = new User(name);
 
-        assertEquals(u.getName(),name);
+        assertEquals(name, u.getName());
     }
 
     @Test
@@ -23,6 +27,42 @@ public class UserUnitTest {
 
         User u = new User(name);
 
-        assertEquals(u.getName(), "");
+        assertEquals("", u.getName());
+    }
+
+    @Test
+    public void createNewDeck() {
+        User u = new User("Bob");
+
+        String deckName = "English animals";
+        u.createNewDeck(deckName);
+
+        assertEquals(deckName, u.getDeck(0).getTitle());
+        assertEquals(0, u.getDeck(0).getDeckSize());
+    }
+
+
+    @Test
+    public void getDeckTitles() {
+        User u = new User("Bob");
+
+        String deckName = "English animals";
+        u.createNewDeck(deckName+"1");
+        u.createNewDeck(deckName+"2");
+        u.createNewDeck(deckName+"3");
+
+        List<String> names = u.getDeckTitles();
+
+        for (int i = 0; i < names.size(); i++) {
+            assertEquals(deckName+(i+1), names.get(i));
+        }
+    }
+
+    @Test
+    public void getDeckInvalidDeck() {
+        User u = new User("Bob");
+        Deck d = u.getDeck(18);
+
+        assertNull(d);
     }
 }
