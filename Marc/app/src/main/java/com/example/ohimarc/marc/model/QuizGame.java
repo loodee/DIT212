d3965dc5734bfc4b3506266e48abd19e08f8bcbc
Vroxie <victor.c.johansson@gmail.com>
2018-10-09 15:54:52 +0200

    Bugfix in QuizGame
    It returns null if nextCard is bigger than the length of the deck

--- Marc/app/src/main/java/com/example/ohimarc/marc/model/QuizGame.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/model/QuizGame.java
@@ -38,25 +38,30 @@ public class QuizGame extends Game {
          */
     @Override
     public String[] peekNextCard() {
-        String[] cardWithAnswer = new String[5];
-        ArrayList<String> answers = new ArrayList<>();
-        rigthAnswerIndex = getRandomIndex();
-        String front = cardCopies[getNextCard()][0];
-        String rightAnswer = cardCopies[getNextCard()][1];
-        cardWithAnswer[0] = front;
-        cardWithAnswer[rigthAnswerIndex] = rightAnswer;
-        for(int i = 0; i < cardCopies.length;i++){
-            answers.add(cardCopies[i][1]);
-        }
-        answers.remove(getNextCard());
-        for(int i = 0; i<cardWithAnswer.length;i++){
-            if(cardWithAnswer[i] == null){
-                int index = getRandomFromList(answers);
-                cardWithAnswer[i] = answers.get(index);
-                answers.remove(index);
+        if (getNextCard() < cardCopies.length) {
+            String[] cardWithAnswer = new String[5];
+            ArrayList<String> answers = new ArrayList<>();
+            rigthAnswerIndex = getRandomIndex();
+            String front = cardCopies[getNextCard()][0];
+            String rightAnswer = cardCopies[getNextCard()][1];
+            cardWithAnswer[0] = front;
+            cardWithAnswer[rigthAnswerIndex] = rightAnswer;
+            for (int i = 0; i < cardCopies.length; i++) {
+                answers.add(cardCopies[i][1]);
             }
+            answers.remove(getNextCard());
+            for (int i = 0; i < cardWithAnswer.length; i++) {
+                if (cardWithAnswer[i] == null) {
+                    int index = getRandomFromList(answers);
+                    cardWithAnswer[i] = answers.get(index);
+                    answers.remove(index);
+                }
+            }
+            return cardWithAnswer;
         }
-        return cardWithAnswer;
+        else{
+            return null;
+        }
     }
 
 
