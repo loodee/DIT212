939972971cd7bc22afa1e2b1e49754424659d65f
Vroxie <victor.c.johansson@gmail.com>
2018-10-10 16:57:58 +0200

    Removed redudant dependicies

--- Marc/app/src/main/java/com/example/ohimarc/marc/view/choosingDeck/ChoosingDeckActivity.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/view/choosingDeck/ChoosingDeckActivity.java
@@ -1,17 +1,9 @@
 package com.example.ohimarc.marc.view.choosingDeck;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.example.ohimarc.marc.R;
 import com.example.ohimarc.marc.presenter.ChoosingDeckPresenter;
 import com.example.ohimarc.marc.view.ToolbarExtension;
