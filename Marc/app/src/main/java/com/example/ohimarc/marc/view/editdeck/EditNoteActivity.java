720649ff59639a647292351c48d444e3a02f4f97
Sorchar <gusmathfo@student.gu.se>
2018-10-12 12:44:46 +0200

    Rearranged import

--- Marc/app/src/main/java/com/example/ohimarc/marc/view/editdeck/EditNoteActivity.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/view/editdeck/EditNoteActivity.java
@@ -1,10 +1,10 @@
 package com.example.ohimarc.marc.view.editdeck;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
