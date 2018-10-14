a6a9bc0fceb7e8b3781acc41b18f7b4e54c6e52a
Alex <axoomax94@hotmail.com>
2018-10-15 00:02:31 +0200

    Commenting ExerciseActivity
    Added author and comments.

--- Marc/app/src/main/java/com/example/ohimarc/marc/view/exerciseView/ExerciseActivity.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/view/exerciseView/ExerciseActivity.java
@@ -1,5 +1,9 @@
 package com.example.ohimarc.marc.view.exerciseView;
 
+/**
+ * @author Alexander Sandberg (alexandersand on github)
+ */
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -28,15 +32,40 @@ public class ExerciseActivity extends ToolbarExtension implements ExerciseView {
         initExtension(this, R.id.activity_exercise, presenter.getDeckTitle());
     }
 
+    /**
+     * This function is called when the "Flashcard Game" button is clicked in the view.
+     * It sets the int modeIndex to 0, which is used in the function navigate later on.
+     * @param v is a View which in this case is the XML-file activity_exercise.
+     */
+
     public void flashcardClicked(View v) {
         modeIndex = 0;
         navigate();
     }
 
+    /**
+     * This function is called when the "Quiz Game" button is clicked in the view.
+     * It sets the int modeIndex to 0, which is used in the function navigate later on.
+     * @param v is a View which in this case is the XML-file activity_exercise.
+     */
+
     public void quizClicked(View v) {
         modeIndex = 1;
         navigate();
     }
+
+    /**
+     * This function is used to handle any navigation from the view. With a case switch,
+     * it uses the int variable modeIndex to set the Intent.
+     * In the case where quiz has been selected, it will need to check whether the current deck
+     * has got more than 3 cards in it in order not to break the program later on.
+     * After the Intent has been set, the function checks whether Intent is null. If it is, the
+     * function should do nothing in order not to break the program. Else, it will pack a bundle,
+     * start the next activity and finish itself.
+     * The function is a boolean to be able to make a return statement and finish the function in
+     * an earlier state.
+     * @return
+     */
 
     @Override
     public boolean navigate() {
@@ -66,12 +95,25 @@ public class ExerciseActivity extends ToolbarExtension implements ExerciseView {
         return true;
     }
 
+    /**
+     * This functions handles the unpacking of a bundle. The bundle has previously been packed
+     * by ChoosingDeckActivity. In the bundle, an int with the key "deckIndex" is expected to
+     * be found, which marks what deck has been selected. This index needs to be forwarded to
+     * the next activity.
+     */
+
     private void unpackBundle() {
         Bundle b = getIntent().getExtras();
         if (b != null) {
             deckIndex = b.getInt("deckIndex");
         }
     }
+
+    /**
+     * This function handles the forwarding of the int deckIndex. It is packed into a bundle
+     * with the key "deckIndex", which is then put into an Intent.
+     * @param intent is an Intent, given by the function navigate(), above.
+     */
 
     private void packBundle(Intent intent) {
         Bundle b = new Bundle();
