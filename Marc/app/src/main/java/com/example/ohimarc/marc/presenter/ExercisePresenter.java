683809fb21915988ac15c4c110bf9aace4b579de
Alex <axoomax94@hotmail.com>
2018-10-15 00:12:29 +0200

    Commenting ExercisePresenter
    Added comments and author.

--- Marc/app/src/main/java/com/example/ohimarc/marc/presenter/ExercisePresenter.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/presenter/ExercisePresenter.java
@@ -2,6 +2,10 @@ package com.example.ohimarc.marc.presenter;
 
 import com.example.ohimarc.marc.model.MemorizationTrainingTool;
 
+/**
+ * @author Alexander Sandberg (alexandersand on github)
+ */
+
 public class ExercisePresenter {
 
     private final MemorizationTrainingTool mtt = MemorizationTrainingTool.getInstance();
@@ -11,10 +15,21 @@ public class ExercisePresenter {
         this.index = index;
     }
 
+    /**
+     * This function talks with the model through MemorisationTrainingTool, and gets the title
+     * of a specific deck.
+     * @return the title of the selected deck, which is used in ExerciseActivity.
+     */
 
     public String getDeckTitle() {
         return mtt.getActiveUser().getDeck(index).getTitle();
     }
+
+    /**
+     * This function talks with the model through MemorisationTrainingTool, and gets the size
+     * of a specific deck.
+     * @return the size of the selected deck, which is used in ExerciseActivity.
+     */
 
     public int getDeckSize() {
         return mtt.getActiveUser().getDeck(index).getDeckSize();
