f9c6afb7e9e28c46c8c4eb4aed288350ed5389d8
Alex <axoomax94@hotmail.com>
2018-10-15 00:29:48 +0200

    Moved functionality to FlashcardPresenter and added comments
    FlashcardPresenter now calls onCreate by itself. Comments and author has been added to FlashcardActivity.

--- Marc/app/src/main/java/com/example/ohimarc/marc/view/flashcardView/FlashcardActivity.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/view/flashcardView/FlashcardActivity.java
@@ -11,6 +11,10 @@ import com.example.ohimarc.marc.R;
 import com.example.ohimarc.marc.view.resultsView.ResultsActivity;
 import com.example.ohimarc.marc.view.toolbarExtensionView.ToolbarExtension;
 
+/**
+ * @author Alexander Sandberg (alexandersand on github)
+ */
+
 public class FlashcardActivity extends ToolbarExtension implements FlashcardView {
 
     private FlashcardPresenter presenter;
@@ -30,19 +34,39 @@ public class FlashcardActivity extends ToolbarExtension implements FlashcardView
         unpackBundle();
 
         presenter = new FlashcardPresenter(this, deckIndex);
-        presenter.onCreate();
         initExtension(this, R.id.flashcard_activity, presenter.getDeckTitle());
     }
+
+    /**
+     * This function initializes the view with some Strings. It is called once, from
+     * FlashcardPresenter.
+     * @param deckTitleText is a String which is the deckTitle of a selected deck.
+     * @param cardText is a String which is the front text of the first card in the deck.
+     */
 
     public void initTexts(String deckTitleText, String cardText) {
         cardButton.setText(cardText);
         cardTitle.setText("Q:");
     }
 
+    /**
+     * This function sets the new text to the card in the view.
+     * @param qOrA is a String, which simply marks the card for when a question or answer
+     *             is displayed with "Q: " or "A: ".
+     * @param text is a String, which is either the back or the front of the current card.
+     */
+
     public void flipCardButton(String qOrA, String text) {
         cardTitle.setText(qOrA);
         cardButton.setText(text);
     }
+
+    /**
+     * This function is called in the XML-file activity_flashcard when the flashcard is clicked.
+     * The function tells the presenter that it has been clicked, while also telling the presenter
+     * if either "Q:" or "A:" is displayed on the card.
+     * @param v is a View which in this case is the XML-file activity_flashcard.
+     */
 
     public void flipCardButtonClicked(View v) {
         boolean bool = false;
@@ -52,13 +76,33 @@ public class FlashcardActivity extends ToolbarExtension implements FlashcardView
         presenter.flashCardClicked(bool);
     }
 
+    /**
+     * This function is called in the XML-file activity_flashcard when the "Correct" button is
+     * clicked. It calls a function in the presenter with the boolean "isCorrect" set as true,
+     * marking that the user has answered the question correctly.
+     * @param v is a View which in this case is the XML-file activity_flashcard.
+     */
+
     public void correctButtonClicked(View v) {
         presenter.resultButtonsClicked(true);
     }
 
+    /**
+     * This function is called in the XML-file activity_flashcard when the "Incorrect" button is
+     * clicked. It calls a function in the presenter with the boolean "isCorrect" set as false,
+     * marking that the user has answered the question incorrectly.
+     * @param v is a View which in this case is the XML-file activity_flashcard.
+     */
+
     public void incorrectButtonClicked(View v) {
         presenter.resultButtonsClicked(false);
     }
+
+    /**
+     * This function handles the navigation to the next activity, namely ResultsActivity.
+     * It is called from FlashcardPresenter when no more cards are available.
+     * It sets the Intent, packs a bundle, starts the activity and finished itself.
+     */
 
     public void changeView() {
         Intent intent = new Intent(FlashcardActivity.this, ResultsActivity.class);
@@ -67,12 +111,24 @@ public class FlashcardActivity extends ToolbarExtension implements FlashcardView
         finish();
     }
 
+    /**
+     * This function unpacks the bundle, where an int is expected to be found with the key
+     * "deckIndex". This int marks which deck has been selected, which is later used in the
+     * FlashcardPresenter.
+     */
+
     private void unpackBundle() {
         Bundle b = getIntent().getExtras();
         if (b != null) {
             deckIndex = b.getInt("deckIndex");
         }
     }
+
+    /**
+     * This function packs a bundle for ResultsActivity. It puts the results into the bundle,
+     * the current game selected, i.e "Flashcard Game", while also forwarding the deckIndex.
+     * @param intent is an Intent, given by the function changeView().
+     */
 
     private void packBundle(Intent intent) {
         Bundle b = new Bundle();
