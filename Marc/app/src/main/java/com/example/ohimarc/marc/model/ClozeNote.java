14df7fb3aa7d1840a2bc450bef9fabf44199fba8
galbertsson <gustav.albertsson@hotmail.com>
2018-10-15 16:16:04 +0200

    Added class skeleton for ClozeNote

---
+++ Marc/app/src/main/java/com/example/ohimarc/marc/model/ClozeNote.java
@@ -0,0 +1,14 @@
+package com.example.ohimarc.marc.model;
+
+public class ClozeNote extends Note {
+
+    @Override
+    void generateCards() {
+        
+    }
+
+    @Override
+    String[][] getCardInfo() {
+        return new String[0][];
+    }
+}
