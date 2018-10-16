82cf3c159832cc774a9421e4f14a9af4cfb7ac72
Alex <axoomax94@hotmail.com>
2018-10-16 15:03:00 +0200

    Adding functionality for achievementsPresenter
    Adding method setAchievements() and adding functionality to onCreate().

--- Marc/app/src/main/java/com/example/ohimarc/marc/presenter/achievementsPresenter.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/presenter/achievementsPresenter.java
@@ -1,10 +1,14 @@
 package com.example.ohimarc.marc.presenter;
 
+import com.example.ohimarc.marc.model.Achievements;
 import com.example.ohimarc.marc.model.MemorizationTrainingTool;
 import com.example.ohimarc.marc.view.achievementsView.AchievementsView;
 
+import java.util.List;
+
 public class AchievementsPresenter {
 
+    private Achievements achievements;
     private AchievementsView view;
     private final MemorizationTrainingTool mtt = MemorizationTrainingTool.getInstance();
 
@@ -15,5 +19,17 @@ public class AchievementsPresenter {
     }
 
     private void onCreate() {
+        achievements = mtt.getActiveUser().getAchievements();
+        setAchievements();
+    }
+
+    private void setAchievements() {
+        List<Achievements.achievements> achis = achievements.getEnumsAsList();
+        for(int i = 0; i < achis.size() ; i++) {
+            if(achievements.getCompletedAchievements().contains(achis.get(i))) {
+                System.out.println(achis.get(i));
+                view.unlockAchievement(i);
+            }
+        }
     }
 }
