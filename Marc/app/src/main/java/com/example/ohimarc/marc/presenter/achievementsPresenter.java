3b278e1e3f3deacedc9d48e6f1b65babccbc920a
Alex <axoomax94@hotmail.com>
2018-10-16 13:02:50 +0200

    Adding skeleton methods for achievementsPresenter

--- Marc/app/src/main/java/com/example/ohimarc/marc/presenter/achievementsPresenter.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/presenter/achievementsPresenter.java
@@ -1,4 +1,19 @@
 package com.example.ohimarc.marc.presenter;
 
+import com.example.ohimarc.marc.model.MemorizationTrainingTool;
+import com.example.ohimarc.marc.view.achievementsView.AchievementsView;
+
 public class AchievementsPresenter {
+
+    private AchievementsView view;
+    private final MemorizationTrainingTool mtt = MemorizationTrainingTool.getInstance();
+
+
+    public AchievementsPresenter(AchievementsView view) {
+        this.view = view;
+        onCreate();
+    }
+
+    private void onCreate() {
+    }
 }
