24286fb35f061fdf6f537d7046d3b69fce23ab2f
Alex <axoomax94@hotmail.com>
2018-10-19 14:36:14 +0200

    Commenting ResultsActivity

--- Marc/app/src/main/java/com/example/ohimarc/marc/view/resultsView/ResultsActivity.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/view/resultsView/ResultsActivity.java
@@ -16,6 +16,8 @@ import java.util.ArrayList;
 
 /**
  * @author Alexander Sandberg (alexandersand on github)
+ * The purpose of this Activity is to set up the Results view, and to handle any user interaction
+ * with the UI. It mainly contains a couple of navigation methods.
  */
 
 public class ResultsActivity extends ToolbarExtension implements ResultsView {
@@ -49,10 +51,11 @@ public class ResultsActivity extends ToolbarExtension implements ResultsView {
 
     /**
      * This function initializes the texts of the view.
+     *
      * @param correct is an int, which marks the amount of correct answers given in the previous
      *                activity.
-     * @param total is an int, which marks the amount of total questions given in the previous
-     *              activity.
+     * @param total   is an int, which marks the amount of total questions given in the previous
+     *                activity.
      */
 
     public void initTexts(int correct, int total) {
@@ -65,6 +68,7 @@ public class ResultsActivity extends ToolbarExtension implements ResultsView {
      * This function is called in the XML-file activity_results when the "Retry" button is clicked.
      * It checks which activity it originates from, and sets an Intent back to that activity.
      * If Intent isn't set to null, it will pack a bundle, start the activity and finish itself.
+     *
      * @param v is a View which in this case is the XML-file activity_results.
      */
 
@@ -92,6 +96,7 @@ public class ResultsActivity extends ToolbarExtension implements ResultsView {
      * This function is called in the XML-file activity_results when the "Return to deck" button
      * is clicked. It will set an Intent to ChoosingDeckActivity, start the activity and
      * finish itself.
+     *
      * @param v is a View which in this case is the XML-file activity_results.
      */
 
@@ -119,6 +124,7 @@ public class ResultsActivity extends ToolbarExtension implements ResultsView {
     /**
      * This function packs a bundle for the next activity. It creates a Bundle b and puts the int
      * deckIndex into it with the key "deckIndex".
+     *
      * @param intent is an Intent which is given by the function retryButton above. This is the
      *               Intent which the bundle is supposed to belong to.
      */
