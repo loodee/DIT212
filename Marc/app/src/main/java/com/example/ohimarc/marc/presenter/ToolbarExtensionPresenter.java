e2dda4d12a9d25bed56095266e36d910ffad3999
Alex <axoomax94@hotmail.com>
2018-10-21 14:34:17 +0200

    Commenting ToolbarExtensionPresenter

--- Marc/app/src/main/java/com/example/ohimarc/marc/presenter/ToolbarExtensionPresenter.java
+++ Marc/app/src/main/java/com/example/ohimarc/marc/presenter/ToolbarExtensionPresenter.java
@@ -17,11 +17,25 @@ public class ToolbarExtensionPresenter {
     private final ToolbarExtensionView view;
     private final UserStorage store;
 
+    /**
+     * This is the constructor of ToolbarExtensionPresenter.
+     *
+     * @param view     is the interface ToolbarExtensionView, which is implemented by ToolbarExtension.
+     *                 This instance is used to call methods in ToolbarExtension.
+     * @param filePath is a file path, stored as a String, provided by ToolbarExtension. It is
+     *                 needed for saving data later on.
+     */
     public ToolbarExtensionPresenter(ToolbarExtensionView view, String filePath) {
         this.view = view;
         this.store = UserStorageFactory.createLocalUserStorage(filePath);
     }
 
+    /**
+     * This method contains functionality for logging out a user. Prior to logging out, the model
+     * needs to be updated with this information. This provides the only purpose of this Presenter.
+     * The user is set to null, the information is stored, and ToolbarExtension is told to navigate
+     * to the login screen.
+     */
     public void logoutButton() {
         MemorizationTrainingTool.getInstance().setActiveUser(null);
         store.storeState(MemorizationTrainingTool.getInstance());
